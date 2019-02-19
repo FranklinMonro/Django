aca0ae92582d6e18a427945bc426ec674d9e2a28
Franco Groenewald <34055502+FranklinMonro@users.noreply.github.com>
2019-02-19 07:12:16 +0200

    Convert from pound to kilogram

--- javaScript_task1_assessment1.js
+++ javaScript_task1_assessment1.js
@@ -1,4 +1,4 @@
 ar pound = prompt("What is the weight in pound")
 var kilogram = pound * 0.454
 alert("the object in kilogram is: " + kilogram)
-console.log("Conversion Completed")
+console.log("Conversion Completed") 

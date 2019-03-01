0dc2d37d72d71b8d6be8537a0dd1f945f833b717
Franco Groenewald <34055502+FranklinMonro@users.noreply.github.com>
2019-03-01 13:47:50 +0200

    Card Game

--- cardGame.py
+++ cardGame.py
@@ -10,37 +10,34 @@ random.shuffle(DECKS)# Shuffle the decks
 player = DECKS[:26]# split deck evenly
 computer = DECKS[26:]# split deck evenly
 
-print("The players card count is: " , len(player))
-print("The computers card count is: ", len(computer))
-plrRandom = random.randint(0, len(player))
-cmpRandom = random.randint(0, len(computer))
-plrCard = player.pop((plrRandom))
-comCard = computer.pop((cmpRandom))
-table_cards = []
-table_cards.append(plrCard)
-table_cards.append(comCard)
-print("The players cards is:" , plrCard)
-print("The computers card is:" , comCard)
-print("The players card count is: " , len(player))
-print("The computers card count is: ", len(computer))
-print("The table card count is: ", len(table_cards))
 
-pinput = input("do you want to play(y/n)")
 
-while pinput != 'n':
-    pinput = input("do you want to play(y/n)")
-    if pinput == 'y':
-        if plrCard[1] == comCard[1]:
-            print("This is war")
-        elif plrCard[1] > comCard[1]:
+
+
+while len(player) != 0 or len(computer) != 0:
+        print("The players card count is: " , len(player))
+        print("The computers card count is: ", len(computer))
+        plrRandom = random.randint(0, len(player)-1)
+        cmpRandom = random.randint(0, len(computer)-1)
+        plrCard = player.pop((plrRandom))
+        comCard = computer.pop((cmpRandom))
+        table_cards = []
+        table_cards.append(plrCard)
+        table_cards.append(comCard)
+        print("The players cards is:" , plrCard)
+        print("The computers card is:" , comCard)
+        print("The players card count is: " , len(player))
+        print("The computers card count is: ", len(computer))
+        print("The table card count is: ", len(table_cards))
+        if plrCard[1] > comCard[1]:
             print("The player has won")
             player.extend(table_cards)
-            computer.pop()
+
             print("The players card count is: " , len(player))
             print("The computers card count is: ", len(computer))
         else:
             print("The computer has won")
             computer.extend(table_cards)
-            player.pop()
+
             print("The players card count is: " , len(player))
             print("The computers card count is: ", len(computer))
